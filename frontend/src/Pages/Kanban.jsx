c1573d86fe7a90cf4ba5c923909e97bc11dedec9
Nimyaah24 <nimyapappy823@gmail.com>
2026-06-17 20:58:16 +0530

    hi

--- frontend/src/Pages/Kanban.jsx
+++ frontend/src/Pages/Kanban.jsx
@@ -1039,7 +1039,7 @@ onClick={() => {
                   >
 
 
-                   <div className="d-flex justify-content-between align-items-center " style={{height:"150px"}}>
+                   <div className="d-flex justify-content-between align-items-center " style={{height:"120px"}}>
   <span
     className="px-3 py-2"
     style={{
